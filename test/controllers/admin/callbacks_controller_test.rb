bf4edb4c474b94ca6641e28ab628baf50088c094
Martin Villalba <martinvillalba@Martins-MacBook-Pro.local>
2025-08-04 14:29:27 -0300

    Fixing test

--- test/controllers/admin/callbacks_controller_test.rb
+++ test/controllers/admin/callbacks_controller_test.rb
@@ -2,22 +2,41 @@ require "test_helper"
 
 describe Admin::CallbacksController do
   it "gets index" do
-    get admin_callbacks_index_url
+    sign_in users(:admin)
+    get admin_callbacks_url
     must_respond_with :success
   end
 
   it "gets show" do
-    get admin_callbacks_show_url
+    sign_in users(:admin)
+    callback = callbacks(:one)
+    get admin_callback_url(callback)
     must_respond_with :success
   end
 
   it "gets edit" do
-    get admin_callbacks_edit_url
+    sign_in users(:admin)
+    callback = callbacks(:one)
+    get edit_admin_callback_url(callback)
     must_respond_with :success
   end
 
   it "gets update" do
-    get admin_callbacks_update_url
-    must_respond_with :success
+    sign_in users(:admin)
+    callback = callbacks(:one)
+    patch admin_callback_url(callback), params: {
+      callback: {
+        url: "https://example.com/updated-callback",
+        timeout_in_seconds: 15,
+        active: true
+      }
+    }
+    must_respond_with :redirect
+  end
+
+  it "posts sync" do
+    sign_in users(:admin)
+    post sync_admin_callbacks_url
+    must_respond_with :redirect
   end
 end
